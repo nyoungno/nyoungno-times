9916b4ec19df635d88b36081fe8cd73298eebe1e
nyoungno Ko <yghjbm@gmail.com>
2024-05-21 02:50:01 +0900

    secondcommit

--- main.js
+++ main.js
@@ -1,9 +1,84 @@
-const API_KEY = ``;
-const getLatestNews = () => {
-  const url = new URL(
-    ` =${API_KEY}`
+const API_KEY = `d87115fdbe2240cb8187821a25a1fe4b`;
+let newsList = [];
+const menus = document.querySelectorAll(".menus button");
+
+menus.forEach((menus) =>
+  menus.addEventListener("click", (event) => getNewsByCategory(event))
+);
+
+let url = new URL(
+  `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
+);
+
+const getNews = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  newsList = data.articles;
+  render();
+};
+
+const getLatestNews = async () => {
+  url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
   );
-  const response = fetch(url);
+  getNews();
+};
+
+const getNewsByCategory = async (event) => {
+  const category = event.target.textContent.toLowerCase();
+  console.log("category", category);
+  url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
+  );
+  getNews();
+};
+
+const searchNews = async () => {
+  const keyword = document.getElementById("search-input").value;
+  url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`
+  );
+  getNews();
+};
+
+const render = () => {
+  const newsHTML = newsList
+    .map(
+      (news) => `<div class="row news">
+  <div class="col-lg-4">
+      <img class="news-img-size" src="${news.urlToImage}">
+  </div>
+  <div class="col-lg-8">
+      <h2>${news.title}</h2>
+      <P>${news.description}</P>
+      <div>${news.source.name} * ${news.publishedAt}</div>
+  </div>
+</div>`
+    )
+    .join("");
+
+  document.getElementById("news-board").innerHTML = newsHTML;
 };
 
 getLatestNews();
+
+const openNav = () => {
+  document.getElementById("mySidenav").style.width = "250px";
+};
+
+const closeNav = () => {
+  document.getElementById("mySidenav").style.width = "0";
+};
+
+const openSearchBox = () => {
+  let inputArea = document.getElementById("input-area");
+  if (inputArea.style.display === "inline") {
+    inputArea.style.display = "none";
+  } else {
+    inputArea.style.display = "inline";
+  }
+};
+
+//1. 버튼들에 클릭이벤트주기
+//2. 카테고리별 뉴스 가져오기
+//3. 그 뉴스를 보여주기
