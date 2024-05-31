f342f7e79206b21294dedda3ec105fcc868126ab
nyoungno Ko <yghjbm@gmail.com>
2024-05-31 13:12:26 +0900

    fourthcommit

--- main.js
+++ main.js
@@ -10,11 +10,31 @@ let url = new URL(
   `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
 );
 
+let totalResults = 0;
+let page = 1;
+const pageSize = 10;
+const groupSize = 5;
+
 const getNews = async () => {
-  const response = await fetch(url);
-  const data = await response.json();
-  newsList = data.articles;
-  render();
+  try {
+    url.searchParams.set("page", page); // => &page=page
+    url.searchParams.set("pageSize", pageSize);
+    const response = await fetch(url);
+    const data = await response.json();
+    if (response.status === 200) {
+      if (data.articles.length === 0) {
+        throw new Error("No result fot this search");
+      }
+      newsList = data.articles;
+      totalResults = data.totalResults;
+      render();
+      paginationRender();
+    } else {
+      throw new Error(data.message);
+    }
+  } catch (error) {
+    errorRender(error.message);
+  }
 };
 
 const getLatestNews = async () => {
@@ -58,6 +78,56 @@ const render = () => {
     .join("");
 
   document.getElementById("news-board").innerHTML = newsHTML;
+};
+
+const errorRender = (errorMessage) => {
+  const errorHTML = `<div class="alert alert-danger" role="alert">
+  ${errorMessage}
+</div>`;
+
+  document.getElementById("news-board").innerHTML = errorHTML;
+};
+
+const paginationRender = () => {
+  //totalResults
+  //page
+  //pageSize
+  //groupSize
+  //totalPages
+  const totalPages = Math.ceil(totalResults / pageSize);
+  //pageGroup
+  const pageGroup = Math.ceil(page / groupSize);
+  //lastPage
+  let lastPage = pageGroup * groupSize;
+  //마지막 페이지그룹이 그룹사이즈보가 작다?
+  if (lastPage > totalPages) {
+    lastPage = totalPages;
+  }
+
+  //firstPage
+
+  const firstPage =
+    lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
+
+  let paginationHTML = `<li class="page-item" onclick="moveToPage(${
+    page - 1
+  })"><a class="page-link" href="#">Previous</a></li>
+  `;
+
+  for (let i = firstPage; i <= lastPage; i++) {
+    paginationHTML += ` <li class="page-item ${
+      i === page ? "active" : ""
+    }" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`;
+  }
+  paginationHTML += `<li class="page-item" onclick="moveToPage(${[
+    page + 1,
+  ]})"><a class="page-link" href="#">Next</a></li>`;
+  document.querySelector(".pagination").innerHTML = paginationHTML;
+};
+
+const moveToPage = (pageNum) => {
+  page = pageNum;
+  getNews();
 };
 
 getLatestNews();
